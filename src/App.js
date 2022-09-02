843bf58e6f6d9608a32560c029aaa7b143f6584b
Ashley Littlewood <ashley.littlewood22@gmail.com>
2022-09-02 11:51:04 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="drum-machine">
+        <div className="display" id="display">
+          <button className="drum-pad" id="Q">Q</button>
+          <button className="drum-pad" id="W">W</button>
+          <button className="drum-pad" id="E">E</button>
+          <button className="drum-pad" id="A">A</button>
+          <button className="drum-pad" id="S">S</button>
+          <button className="drum-pad" id="D">D</button>
+          <button className="drum-pad" id="Z">Z</button>
+          <button className="drum-pad" id="X">X</button>
+          <button className="drum-pad" id="C">C</button>
+        </div>
+      </div>
     </div>
   );
 }
