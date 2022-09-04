04a0e411790a6fb648f17ef31ce225a4a123be70
Ashley Littlewood <ashley.littlewood22@gmail.com>
2022-09-04 23:02:35 +0100

    Added Title & display sound text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+
+  const [displaySound, setDisplaySound] = useState();
+
   return (
     <div className="App">
       <div id="drum-machine">
         <div className="display" id="display">
+        <h1 className="title">Drum Machine</h1>
           <button className="drum-pad" id="Q">Q<audio className="clip" id="Q"></audio></button>
           <button className="drum-pad" id="W">W<audio className="clip" id="W"></audio></button>
           <button className="drum-pad" id="E">E<audio className="clip" id="E"></audio></button>
@@ -14,6 +19,7 @@ function App() {
           <button className="drum-pad" id="Z">Z<audio className="clip" id="Z"></audio></button>
           <button className="drum-pad" id="X">X<audio className="clip" id="X"></audio></button>
           <button className="drum-pad" id="C">C<audio className="clip" id="C"></audio></button>
+        <div className="display-sound">{displaySound}</div>
         </div>
       </div>
     </div>
